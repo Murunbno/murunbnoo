1ee11653288c8c82d3b8e342e388725595f5a9e9
Admin <Admin@DESKTOP-OI5FUJ9>
2024-04-03 10:47:28 +0800

    jargal

--- murun/src/murun/murunbho.java
+++ murun/src/murun/murunbho.java
@@ -5,8 +5,8 @@ public class murunbho {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellow");
-	
 		System.out.println("Hi hi yumgui dee odoo jaahan zavgu bna ");
+		
 	}
 
 }
