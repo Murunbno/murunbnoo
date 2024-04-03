a2ad59e33784c460db0e8629cba646e0d262daa8
Admin <Admin@DESKTOP-OI5FUJ9>
2024-04-03 10:38:28 +0800

    second commit

--- murun/src/murun/murunbho.java
+++ murun/src/murun/murunbho.java
@@ -5,6 +5,7 @@ public class murunbho {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellow");
+		System.out.println("Hi hi yumgui dee odoo jaahan zavgu bna ");
 	}
 
 }
